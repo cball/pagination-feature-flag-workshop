da0f0bf59e2190714e8674cc3b4552dc83173d63
Chris Ball <chris@echobind.com>
2015-05-05 15:56:57 -0600

    add sass and image

--- app/pods/restaurant/model.js
+++ app/pods/restaurant/model.js
@@ -1,6 +1,11 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  image: DS.attr('string')
+
+  image: Ember.computed('id', function() {
+    let id = this.get('id');
+    return `http://lorempixel.com/400/200/food/${id}/`;
+  })
 });
