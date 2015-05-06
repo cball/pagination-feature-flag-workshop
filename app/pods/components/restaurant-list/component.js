ce139091725ab564a2839219daf238eacd680287
Chris Ball <chris@echobind.com>
2015-05-06 16:57:29 -0600

    add another addon

--- app/pods/components/restaurant-list/component.js
+++ app/pods/components/restaurant-list/component.js
@@ -4,10 +4,19 @@ import Ember from 'ember';
   {{restaurant-list restaurants=restaurants}}
 */
 export default Ember.Component.extend({
-  restaurants: [],
+  // restaurants: [],
   totalRows: 0,
   page: 1,
   perPage: 10,
+
+  chartDataset: Ember.computed.map('restaurants', function(restaurant) {
+    return {
+      name: restaurant.get('name'),
+      data: [1, 2, 18, 33]
+    }
+  }),
+
+  categories: Ember.computed.mapBy('restaurants', 'name'),
 
   actions: {
     changePage(page) {
